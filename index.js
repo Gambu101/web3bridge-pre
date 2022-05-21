4eda7811a95bed4f16998d2b1b2bb3579c278974
Gambu101 <gabriel.ohalete.244627@unn.edu.ng>
2022-05-21 16:30:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let interest = document.getElementById("interest")
+let tier1 =document.getElementById("tier1")
+let tier2 = document.getElementById("tier2");
+let tier3 = document.getElementById("tier3");
+let amount = document.getElementById("ampount")
+
+const tier1 = ()=>{
+    tipPerc;
+    interest.innerhtml = tt1;
+}
+const t2 = ()=>{
+    return tier2.value
+}
+const t3 =()=>{
+    return tier3.value
+}
